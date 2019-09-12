2355dd53637e6495d35e6b0240bf38547fb8aa88
BuildTools <unconfigured@null.spigotmc.org>
2019-09-12 21:35:22 +0800

    Compilable

--- main.py
+++ main.py
@@ -3,26 +3,27 @@ import colors
 from classes import *
 import interface
 import gameEngine
-pygame.init()
+def codeRun():
+    pygame.init()
 
-screen = pygame.display.set_mode((700, 700))
+    screen = pygame.display.set_mode((700, 700))
 
-pygame.display.set_caption('Python Agario')
+    pygame.display.set_caption('Python Agario')
 
-run = True
-player = gameEngine.playerEngine(screen)
-scoreboard = Score()
-dotGroup = gameEngine.dotEngine(screen, player.Player.rect)
-while run:
-    pygame.time.delay(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-              run = False
-    screen.fill(colors.azure)
+    run = True
+    player = gameEngine.playerEngine(screen)
+    scoreboard = Score()
+    dotGroup = gameEngine.dotEngine(screen, player.Player.rect)
+    while run:
+        pygame.time.delay(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                  run = False
+        screen.fill(colors.azure)
 
-    dotGroup.rectUpdate(player.Player.rect)
-    dotGroup.pos_check(scoreboard, player)
-    dotGroup.draw()
-    player.draw()
-    interface.gui(screen, scoreboard)
-    pygame.display.update()
+        dotGroup.rectUpdate(player.Player.rect)
+        dotGroup.pos_check(scoreboard, player)
+        dotGroup.draw()
+        player.draw()
+        interface.gui(screen, scoreboard)
+        pygame.display.update()
